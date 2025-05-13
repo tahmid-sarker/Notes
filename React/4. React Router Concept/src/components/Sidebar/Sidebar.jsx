115c976166cba610943e319d8ceb50124dd3b17a
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2025-05-13 13:41:37 +0600

    feat(Sidebar): add Sidebar component with navigation links

---
+++ React/4. React Router Concept/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <nav>
+                <p>Side Bar - 1</p>
+                <p>Side Bar - 2</p>
+                <p>Side Bar - 3</p>
+                <p>Side Bar - 4</p>
+                <p>Side Bar - 5</p>
+            </nav>
+        </aside>
+    );
+};
+
+export default Sidebar;
