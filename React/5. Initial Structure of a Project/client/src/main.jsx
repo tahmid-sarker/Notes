cf0684b2c4b8e323a77528620b547f49c64392ff
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2025-05-24 01:53:19 +0600

    init(client): setup React client

---
+++ React/5. Initial Structure of a Project/client/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router'
+import { router } from './routes/Router'
+import ThemeProvider from './context/ThemeProvider'
+import AuthProvider from './context/AuthProvider'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ThemeProvider>
+  </StrictMode>,
+)
